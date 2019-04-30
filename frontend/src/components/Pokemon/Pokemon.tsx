e550334200bbd8f8f56285b45394b816899ba178
oussamah <oussamah.jaber@manomano.com>
2019-04-30 16:57:59 +0200

    refacto: transforms Pokemon react component to a functional compoenent

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -9,35 +9,27 @@ interface Props {
   height: number;
 }
 
-class Pokemon extends React.Component<Props> {
-  displayName(): string {
-    return this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1);
-  }
-
-  render(): React.ReactNode {
-    const pokemon = 'Carapuce';
-
-    return (
-      <Style.Intro>
-        <div>{this.displayName()}</div>
-        <img
-          alt={this.props.name}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            this.props.id
-          }.png`}
-        />
-        <div>
-          Id: <span className='numberElement'>{this.props.id}</span>
-        </div>
-        <div>
-          Weight: <span className='numberElement'>{this.props.weight}</span> kg
-        </div>
-        <div>
-          Height: <span className='numberElement'>{this.props.height}</span> cm
-        </div>
-      </Style.Intro>
-    );
-  }
+function Pokemon(props: Props) {
+  return (
+    <Style.Intro>
+      <div>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</div>
+      <img
+        alt={props.name}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          props.id
+        }.png`}
+      />
+      <div>
+        Id: <span className="numberElement">{props.id}</span>
+      </div>
+      <div>
+        Weight: <span className="numberElement">{props.weight}</span> kg
+      </div>
+      <div>
+        Height: <span className="numberElement">{props.height}</span> cm
+      </div>
+    </Style.Intro>
+  );
 }
 
 export default Pokemon;
