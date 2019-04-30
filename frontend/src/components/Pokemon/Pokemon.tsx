6cc17879151b210bf956a99cdb9b868802424088
oussamah <oussamah.jaber@manomano.com>
2019-04-30 16:57:59 +0200

    refacto: transforms Pokemon react component to a functional compoenent

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -6,37 +6,27 @@ interface Props {
   pokemonProp: { name: string; id: number; weight: number; height: number };
 }
 
-class Pokemon extends React.Component<Props> {
-  displayName(): string {
-    return (
-      this.props.pokemonProp.name.charAt(0).toUpperCase() + this.props.pokemonProp.name.slice(1)
-    );
-  }
-
-  render(): React.ReactNode {
-    const pokemon = 'Carapuce';
-
-    return (
-      <Style.Intro>
-        <div>{this.displayName()}</div>
-        <img
-          alt={this.props.pokemonProp.name}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            this.props.pokemonProp.id
-          }.png`}
-        />
-        <div>
-          Id: <span className="numberElement">{this.props.pokemonProp.id}</span>
-        </div>
-        <div>
-          Weight: <span className="numberElement">{this.props.pokemonProp.weight}</span> kg
-        </div>
-        <div>
-          Height: <span className="numberElement">{this.props.pokemonProp.height}</span> cm
-        </div>
-      </Style.Intro>
-    );
-  }
+function Pokemon(props: Props) {
+  return (
+    <Style.Intro>
+      <div>{props.pokemonProp.name.charAt(0).toUpperCase() + props.pokemonProp.name.slice(1)}</div>
+      <img
+        alt={props.pokemonProp.name}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          props.pokemonProp.id
+        }.png`}
+      />
+      <div>
+        Id: <span className="numberElement">{props.pokemonProp.id}</span>
+      </div>
+      <div>
+        Weight: <span className="numberElement">{props.pokemonProp.weight}</span> kg
+      </div>
+      <div>
+        Height: <span className="numberElement">{props.pokemonProp.height}</span> cm
+      </div>
+    </Style.Intro>
+  );
 }
 
 export default Pokemon;
